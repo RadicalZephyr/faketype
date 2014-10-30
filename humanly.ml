2de54ed0d6c6f3382e12b3fae09db011823736e1
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-30 15:56:42 -0400

    Add optional argument for setting typing speed

--- humanly.ml
+++ humanly.ml
@@ -4,22 +4,22 @@ type typist =
   | Best | Good | Average | Poor | Worst
 
 
-let get_average_time_for_typist t =
-  match t with
+let get_average_time_for_typist typist =
+  match typist with
   | Best    -> 0.08
   | Good    -> 0.12
   | Average -> 0.2
   | Poor    -> 0.28
   | Worst   -> 1.2
 
-let get_next_span () =
-  Time.Span.of_float 0.1
+let get_next_span typist =
+  Time.Span.of_float (get_average_time_for_typist typist)
 
-let iter ~f str =
+let iter ?(typist=Good) ~f str =
   let rec iter i last =
     match i < last with
     | true  ->
-       Time.pause (get_next_span ());
+       Time.pause (get_next_span typist);
        f (String.get str i);
        iter (i+1) last
     | false -> ()
