4c68bae91f2bf180b8cfb01ffda6c65befc54025
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-30 14:57:24 -0400

    Print character by character

--- faketype.ml
+++ faketype.ml
@@ -1,9 +1,12 @@
 open Core.Std
 
+let flush_char char =
+  Out_channel.output_char stdout char;
+  Out_channel.flush stdout
 
 let () =
   match Array.to_list Sys.argv with
   | [] -> assert false
   | _ :: args ->
      let arg_string = String.concat args ~sep:" " in
-     print_string arg_string
+     String.iter arg_string ~f:flush_char
